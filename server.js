6b2c79cf338f7e3ba7dce9fc02ff32946ada0b43
Denis Moura <drmouradap@gmail.com>
2020-02-18 11:58:47 -0300

    adding application

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require("express")
+const server = express()
+
+
+//configuração do servidor para apresentar arquivos estáticos
+server.use(express.static('public'))
+
+//habilitar body do formulário
+server.use(express.urlencoded({ extended: true }))
+
+//configuração do banco de dados Postgre
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'denis',
+    password: '0000',
+    host: 'localhost',
+    port: 5432,
+    database: 'maratonaDev'
+})
+
+
+//configuração do template engine com nunjucks
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server
+})
+
+
+
+//configuração da página
+server.get("/", function(req, res) {
+    
+    db.query("SELECT * FROM donors", function(err, result) {
+        if (err) return res.send("Erro no banco de dados.")
+
+        // Fluxo ideal
+        const donors = result.rows
+        return res.render("index.html", { donors })
+
+    })
+
+})
+
+server.post("/", function(req, res) {
+    //pegar dados do formulário
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood 
+
+    if (name == "" || email == "" || blood == "") {
+        return res.send("Todos os campos são obrigatórios.")
+    }
+
+    //colocar valores no banco de dados
+    const query = `
+        INSERT INTO donors("name", "email", "blood")
+        VALUES ($1, $2, $3)
+         `
+    const values = [name, email, blood]
+
+    db.query(query, values, function(err) {
+        //fluxo de erro
+        if (err) return res.send("Erro no banco de dados.")
+
+        //fluxo ideal
+        return res.redirect("/")
+    })
+
+
+
+})
+
+
+//ligar o servidor e permitir acesso na porta 3000
+server.listen(3000, function() {
+    console.log("Iniciei o servidor.")
+})
